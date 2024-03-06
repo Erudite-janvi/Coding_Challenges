a17285e9efd121d19bbcd07dd9a95e48d41c07cd
Janvi sharma <janvisharma@Janvis-MacBook-Pro.local>
2024-03-06 08:04:25 -0500

    Day4modified

--- Day4/remove_duplicates.py
+++ Day4/remove_duplicates.py
@@ -6,8 +6,21 @@
 #     return unique_elements
 # array = [2,3,7,4,3,1,4,4,8]   
 # print(remove_duplicate(array))     
-def remove_duplicate(array):
-    unique_list = list(set(array))
-    return unique_list
-array = [2,3,7,4,3,1,4,4,8]
-print(remove_duplicate(array))
+# def remove_duplicate(array):
+#     unique_list = list(set(array))
+#     return unique_list
+# array = [2,3,7,4,3,1,4,4,8]
+# print(remove_duplicate(array))
+def count_duplicates(array):
+    element_count = {}
+    
+    for element in array:
+        if element in element_count:
+            element_count[element] += 1
+        else:
+            element_count[element] = 1
+    
+    return element_count
+
+array = [2, 3, 7, 4, 3, 1, 4, 4, 8]
+print(count_duplicates(array))
