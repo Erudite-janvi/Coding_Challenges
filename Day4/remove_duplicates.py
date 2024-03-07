07cb4ccd1d2814ed246db80bf9f18c24391fcba7
Janvi sharma <janvisharma@Janvis-MacBook-Pro.local>
2024-03-07 07:34:27 -0500

    modifiedcode

--- Day4/remove_duplicates.py
+++ Day4/remove_duplicates.py
@@ -3,14 +3,15 @@
 #     for element in array:
 #         if element not in unique_elements:
 #             unique_elements.append(element)
-#     return unique_elements
+#     return len(unique_elements)
 # array = [2,3,7,4,3,1,4,4,8]   
 # print(remove_duplicate(array))     
 # def remove_duplicate(array):
 #     unique_list = list(set(array))
-#     return unique_list
+#     return len(unique_list)
 # array = [2,3,7,4,3,1,4,4,8]
 # print(remove_duplicate(array))
+#
 def count_duplicates(array):
     element_count = {}
     
--- Day4/second_largest.py
+++ Day4/second_largest.py
@@ -5,10 +5,10 @@
 # a1= second_largest(array)
 # print(a1)
 
-# def second_largest(array):
-#     array.sort()
-#     del array[-1]
-#     return max(array)
-# array =[3,5,4,2,4,10,9]
-# a1= second_largest(array)
-# print(a1)
+def second_largest(array):
+    array.sort()
+    del array[-1]
+    return max(array)
+array =[3,5,4,2,4,10,9]
+a1= second_largest(array)
+print(a1)
